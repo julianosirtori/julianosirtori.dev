255743a669e1158a744571cdbe5dfd709f80bde6
Juliano Sirtori <julianosirtori@gmail.com>
2023-07-14 00:33:11 -0300

    wip: started the presentation component

---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { Presentation } from "./sections/Presentation"
+
+export const Home = () => {
+  return (
+    <div className="w-full flex flex-col">
+      <Presentation />
+    </div>
+  )
+}
---
+++ src/components/pages/Home/sections/Presentation.tsx
@@ -0,0 +1,29 @@
+import { ImageSVG } from "@/components/shared/ImageSVG"
+import Image from "next/image"
+
+export const Presentation = () => {
+  return (
+    <section className="flex flex-col gap-6 p-4 mt-5 w-full pb-11">
+      <div className="w-full flex flex-col gap-6">
+        <h1 className="font-semibold text-2xl text-white max-w-sm">Juliano is a <span className="text-primary">front-end web developer</span></h1>
+        <p className="text-gray ">He crafts responsive websites where technologies meet creativity</p>
+      </div>
+      <div className="flex w-full flex-col items-center">
+        <div className="relative w-full h-72 max-w-sm">
+          <ImageSVG name="logo" width="104px" height="104px" className="z-0 absolute top-10 left-[-8px]" />
+          <Image src="/images/Juliano.png" fill alt="Juliano`s photo" className="absolute z-1" />
+          <ImageSVG name="dots" width="56px" height="56px" className="z-2 absolute bottom-8 right-4" />
+        </div>
+        <div className="border border-gray p-2 flex flex-row gap-2 w-full">
+          <div className="w-4 h-4 bg-primary" />
+          <div className="flex flex-col">
+            <span className="text-gray font-semibold">Currently working on</span>
+            <span className="text-white font-bold">Currently working Portfolio</span>
+          </div>
+        </div>
+      </div>
+
+
+    </section>
+  )
+}
