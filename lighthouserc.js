c4305c8c0d223ff01597fc9aec56262c9c30b202
Juliano Sirtori <julianosirtori@gmail.com>
2023-11-05 15:40:13 -0300

    ci: add news urls to collect

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,15 @@
 module.exports = {
   ci: {
     collect: {
-      url: ["http://localhost:3000/"],
+      url: [
+        "http://localhost:3000/",
+        "http://localhost:3000/en",
+        "http://localhost:3000/pt",
+        "http://localhost:3000/pt/blog",
+        "http://localhost:3000/pt/about",
+        "http://localhost:3000/pt/contact",
+        "http://localhost:3000/pt/projects",
+      ],
       startServerCommand: "npm run start",
     },
     upload: {
