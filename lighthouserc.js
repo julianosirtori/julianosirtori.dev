8e691ad53bec107574a18f5363b15af301e522fe
Juliano Sirtori <julianosirtori@gmail.com>
2023-11-05 15:24:11 -0300

    ci: add new env LHCI_GITHUB_APP_TOKEN

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ["http://localhost:3000/"],
+      startServerCommand: "npm run start",
+    },
     upload: {
       target: "temporary-public-storage",
     },
